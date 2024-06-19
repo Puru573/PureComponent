3e12696e488407b1ea361860695bcf36c1bb58da
Puru573 <purukumar.p@gmail.com>
2024-06-19 14:24:57 +0530

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, PureComponent } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends PureComponent {
+  constructor(){
+    super();
+    this.state={
+      count:{
+        value:1
+      }
+    }
+    //shouldcompoenetupdate say it not that'why is stops
+  }
+  render() {
+    console.log("render");
+    return (
+      <div>
+        <h1>{this.state.count}</h1>
+        <button onClick={
+          ()=>this.setState({
+          count:{value:1}})}
+          >button</button>  
+      </div>
+    )
+  }
 }
-
-export default App;
